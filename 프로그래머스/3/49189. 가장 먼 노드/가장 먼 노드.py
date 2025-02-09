c800bb319116d18156000c30c45c4c75c97ad9e7
seung_gu <127372942+seungguJ@users.noreply.github.com>
2025-02-10 00:50:19 +0900

    [level 3] Title: 가장 먼 노드, Time: 8905.63 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,4 +1,3 @@
-from collections import deque
 def solution(n, edge):
     answer = 0
     '''
@@ -6,41 +5,18 @@ def solution(n, edge):
     n: number of node
     edge: 2 dim of matrix
     '''
+    '''
+    1부터 시작해서 1-n의 숫자 중에서 지우면서 출발하기
+    1 -> 2,3 then, remained: (4,5,6)
+    '''
     
+    remained_node = list(range(1,n+1))
     edge_dict = {}
     for ele in edge:
-        
-        if ele[0] not in edge_dict:
-            edge_dict[ele[0]] = []
-        if ele[1] not in edge_dict:
-            edge_dict[ele[1]] = []
-        
-        edge_dict[ele[0]].append(ele[1])
-        edge_dict[ele[1]].append(ele[0])
-        
-    def bfs(graph, start):
-        queue = deque([start])
-        visited = set([start])
-        depth_dict = {start: 0}  # 시작 노드의 깊이는 0
-
-        while queue:
-            current = queue.popleft()
-
-            for neighbor in graph.get(current, []):
-                if neighbor not in visited:
-                    queue.append(neighbor)
-                    visited.add(neighbor)
-                    depth_dict[neighbor] = depth_dict[current] + 1  # 부모 깊이 + 1로 설정
-
-        return depth_dict
-
-    result = bfs(edge_dict, 1)
-    answer_list = []
-    for key in result:
-        if result[key] == max(result.values()):
-            answer_list.append(key)
-    answer = len(answer_list)
-    
-    
-    
+        ele.sort()
+        if ele[0] not in edge_dict.keys():
+            edge_dict[ele[0]] = [ele[1]]
+        else:
+            edge_dict[ele[0]].append(ele[1])
+    print(edge_dict)
     return answer
