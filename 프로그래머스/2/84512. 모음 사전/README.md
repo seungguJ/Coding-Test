524a73f55edcd5891d11af67b5d9127b5ea85408
seung_gu <127372942+seungguJ@users.noreply.github.com>
2025-03-21 23:58:22 +0900

    [level 2] Title: 모음 사전, Time: 1.89 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 0.01 ms
+메모리: 9.45 MB, 시간: 1.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 23:24:52
+2025년 03월 21일 23:58:21
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -7,20 +7,35 @@ def solution(word):
     
     AE -> (5*5*5*5+5*5*5+5*5+5)+1 + 1 + (5*5*5+5*5+5)+1
     '''
+    # word_list = ["A","E","I","O","U"]
+    # temp=[0 for i in range(5)]
+    # for i in range(len(word)):
+    #     answer+=1
+    #     idx = word_list.index(word[i])
+    #     if i == 0:
+    #         answer += 781*idx
+    #     elif i == 1:
+    #         answer += 156*idx
+    #     elif i == 2:
+    #         answer += 31*idx
+    #     elif i == 3:
+    #         answer += 6*idx
+    #     else:
+    #         answer += idx
+
+    '''
+    recursive
+    
+    '''
     word_list = ["A","E","I","O","U"]
-    temp=[0 for i in range(5)]
-    for i in range(len(word)):
-        answer+=1
-        idx = word_list.index(word[i])
-        if i == 0:
-            answer += 781*idx
-        elif i == 1:
-            answer += 156*idx
-        elif i == 2:
-            answer += 31*idx
-        elif i == 3:
-            answer += 6*idx
-        else:
-            answer += idx
-            
+    
+    def dfs(word_list, current_word, visited):
+        visited.append(current_word)
+        if len(current_word) == 5:
+            return
+        for i in word_list:
+            dfs(word_list, current_word+i, visited)
+    visited = []
+    dfs(word_list, "", visited)
+    answer = visited.index(word)
     return answer
